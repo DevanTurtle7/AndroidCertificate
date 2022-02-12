29c6b09811c656daf1fbdd8f51aeff5eeae4a796
Devan Kavalchek <devankav7@gmail.com>
2022-02-12 16:30:55 -0500

    completed debugging

--- Unit1/Debugging/app/src/main/java/com/devankav/debugging/MainActivity.kt
+++ Unit1/Debugging/app/src/main/java/com/devankav/debugging/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.devankav.debugging
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val helloTextView: TextView = findViewById(R.id.division_textview)
+        helloTextView.text = "Hello, debugging!"
         setContentView(R.layout.activity_main)
+        division()
+    }
+
+     fun division() {
+         val numerator = 60
+         var denominator = 4
+
+         repeat(5) {
+             Thread.sleep(1)
+             Log.v(TAG, "${numerator / denominator}")
+             denominator--
+         }
+     }
+
+    fun logging() {
+        Log.e(TAG, "ERROR: A serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
     }
 }
