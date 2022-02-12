1a01c32a96c5cefac6ee2418ae816500777a3cc1
Devan Kavalchek <devankav7@gmail.com>
2022-02-11 23:01:52 -0500

    reformated code

--- Unit1/DiceRoller/app/src/main/java/com/devankav/diceroller/MainActivity.kt
+++ Unit1/DiceRoller/app/src/main/java/com/devankav/diceroller/MainActivity.kt
@@ -12,20 +12,27 @@ class Dice(private val numSides: Int) {
     }
 }
 
+/**
+ * Allows the button to roll a dice and view the result
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button);
-        rollButton.setOnClickListener{
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Roll the dice and update the text with the result
+     */
     private fun rollDice() {
+        // Create and roll a new 6 sided dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
+
+        // Display the result
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
